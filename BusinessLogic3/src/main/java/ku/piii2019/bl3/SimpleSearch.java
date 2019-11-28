ee12b9fff0e725e8ef33555a42ef99b1b0fd9bdc
Joshua Moore <K1720552@kingston.ac.uk>
2019-11-28 14:53:17 +0000

    Additions added to find() method including try/catch and checking for whitespace.

--- BusinessLogic3/src/main/java/ku/piii2019/bl3/SimpleSearch.java
+++ BusinessLogic3/src/main/java/ku/piii2019/bl3/SimpleSearch.java
@@ -18,17 +18,30 @@ public class SimpleSearch implements SearchService{
     public Set<MediaItem> find(String thisSearchTerm, Set<MediaItem> inhere) {
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
         Set<MediaItem> result = new HashSet<>();
-        for(MediaItem item : inhere){
-            if(item.getArtist().toLowerCase().contains(thisSearchTerm)||
-               item.getAlbum().toLowerCase().contains(thisSearchTerm)||
-               item.getTitle().toLowerCase().contains(thisSearchTerm)){
-                result.add(item);
+        Set<MediaItem> temp = new HashSet<>();
+        try{
+        for(int i=0;i<thisSearchTerm.length();i++){
+            if(thisSearchTerm.charAt(i)==' '){
+                break;
             }
-            if(item.getArtist().contains(thisSearchTerm)||
-               item.getAlbum().contains(thisSearchTerm)||
-               item.getTitle().contains(thisSearchTerm)){
-                result.add(item);
+            for(MediaItem item : inhere){
+                if(item.getArtist().toLowerCase().contains(thisSearchTerm)||
+                    item.getAlbum().toLowerCase().contains(thisSearchTerm)||
+                    item.getTitle().toLowerCase().contains(thisSearchTerm)){
+                    result.add(item);
+                }
+                if(item.getArtist().contains(thisSearchTerm)||
+                    item.getAlbum().contains(thisSearchTerm)||
+                    item.getTitle().contains(thisSearchTerm)){
+                    result.add(item);
+                }
             }
+        }
+        /*if(thisSearchTerm==" "||thisSearchTerm==""){
+            return temp;
+        }*/
+        }catch(NullPointerException e){
+            
         }
         return result;
     }
