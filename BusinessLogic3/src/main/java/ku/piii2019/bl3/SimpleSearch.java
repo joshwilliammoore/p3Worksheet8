143f121f6f22b864e52365735669eaa6800c1c44
Joshua Moore <K1720552@kingston.ac.uk>
2019-11-28 13:29:09 +0000

    find() method implemented, passing all test cases.

--- BusinessLogic3/src/main/java/ku/piii2019/bl3/SimpleSearch.java
+++ BusinessLogic3/src/main/java/ku/piii2019/bl3/SimpleSearch.java
@@ -16,7 +16,21 @@ public class SimpleSearch implements SearchService{
 
     @Override
     public Set<MediaItem> find(String thisSearchTerm, Set<MediaItem> inhere) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Set<MediaItem> result = new HashSet<>();
+        for(MediaItem item : inhere){
+            if(item.getArtist().toLowerCase().contains(thisSearchTerm)||
+               item.getAlbum().toLowerCase().contains(thisSearchTerm)||
+               item.getTitle().toLowerCase().contains(thisSearchTerm)){
+                result.add(item);
+            }
+            if(item.getArtist().contains(thisSearchTerm)||
+               item.getAlbum().contains(thisSearchTerm)||
+               item.getTitle().contains(thisSearchTerm)){
+                result.add(item);
+            }
+        }
+        return result;
     }
     
 }
